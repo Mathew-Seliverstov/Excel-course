c736cef9b8a8514507a13fc528d0adbd2e3877d9
Mathew <matvansel@gmail.com>
2021-05-06 14:05:51 +0300

    Finish project config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+	parser: '@babel/eslint-parser',
+	parserOptions: {
+		babelOptions: {
+			configFile: './babel.config.json'
+		}
+	},
+	env: {
+		browser: true,
+		node: true,
+		es6: true
+	},
+	extends: ['eslint:recommended', 'google'],
+	rules: {
+		'semi': 'off',
+		'comma-dangle': 'off',
+		'require-jsdoc': 'off',
+		'indent': ['error', 'tab'],
+		'no-tabs': ['error', {'allowIndentationTabs': true}],
+		'linebreak-style': 'off'
+	}
+}
